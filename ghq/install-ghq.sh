23348f1b1e1fa32bfbcb177d3a779f1d019a8b55
sasaplus1 <sasaplus1@gmail.com>
2025-03-03 10:06:33 +0900

    some changes

---
+++ ghq/install-ghq.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+url=
+bin="$HOME/.local/bin"
+
+case "$(uname)" in
+  aarch64*|armv*)
+    # TODO: renovate
+    url='https://github.com/x-motemen/ghq/releases/download/v1.7.1/ghq_linux_arm64.zip'
+    ;;
+  i*86|x86_64)
+    # TODO: renovate
+    url='https://github.com/x-motemen/ghq/releases/download/v1.7.1/ghq_linux_amd64.zip'
+    ;;
+esac
+
+mkdir -p "$bin"
+
+[ -n "$url" ] && curl -fsSL --progress-bar --output-dir "$bin" -O "$url"
+
+mkdir -p "$bin"
+unzip -j "$bin/ghq_linux_a??64.zip" '*/ghq' -d "$bin"
+chmod +x "$bin/ghq"
+rm -f "$bin/ghq_linux_a??64.zip"
