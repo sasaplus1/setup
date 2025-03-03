49a048db7c7d8e3d8442799fd5a126202d088b28
sasaplus1 <sasaplus1@gmail.com>
2025-03-03 10:28:26 +0900

    fix

--- ubuntu/setup-gnome.sh
+++ ubuntu/setup-gnome.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change directory names in home directory for Ubuntu
-LANG=C xdg-user-dirs-gtk-update --force
+LANG=C xdg-user-dirs-update --force
 
 # Change UI keybind to Emacs for GNOME on Ubuntu
 gsettings set org.gnome.desktop.interface gtk-key-theme Emacs
